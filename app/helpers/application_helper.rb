e64244cbaaf42287990c0db9f3b56901beb85bb8
Osho957 <osho.personal@gmail.com>
2024-10-13 10:20:18 +0530

    Created Test Ruby On Rails Project

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+end
+
+# helpers folder is for view helpers and is automatically included in views
+# helpers are methods that are available to all views
+# helpers are used to keep views clean and to keep logic out of views
+
+# To include a helper in a view, use the helper method
